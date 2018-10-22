3696a46b23b5b95973dd7449596fc01db0abd86e
Megan Eagan <Megan.Eagan@gmail.com>
2018-10-22 19:54:31 -0400

    added search

---
+++ controller.js
@@ -0,0 +1,34 @@
+// "use strict"; {
+//     angular.module('app')
+//         .controller('movieSearch', function () {
+//             const $ctrl = this;
+//         });
+//     }
+
+
+app.controller("movieSearch", function($scope, $http) {
+ 
+    var pendingTask;
+    
+    if($scope.search === undefined){
+      $scope.search = "Fight Club";
+      getThings();
+    }
+    
+    $scope.change = function(){
+     if(pendingTask){
+        clearTimeout(pendingTask);
+      }
+      pendingTask = setTimeout(getData, 800);
+    };
+    
+
+    function getThings() {
+        return $http.get("https://api.themoviedb.org/3/movie/550?api_key=4f4565d06d2dff19a79a1c507b52ff3d");
+        }
+    
+        getThings().then((responseData) => {
+        console.log(responseData);
+        });
+
+});
